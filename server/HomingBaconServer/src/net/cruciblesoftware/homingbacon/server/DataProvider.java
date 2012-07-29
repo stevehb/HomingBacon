4cd45399ca1ec6b83f95a97e1b0ade04103b468b
Stephen Blackwell <stevehb@gmail.com>
2012-07-28 20:48:14 -0500

    refactored common constants out of server to serparate shared project

--- server/HomingBaconServer/src/net/cruciblesoftware/homingbacon/server/DataProvider.java
+++ server/HomingBaconServer/src/net/cruciblesoftware/homingbacon/server/DataProvider.java
@@ -1,5 +1,7 @@
 package net.cruciblesoftware.homingbacon.server;
 
+import net.cruciblesoftware.homingbacon.DatastoreNames;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -10,16 +12,6 @@ import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 
 class DataProvider {
-    private static final String KIND_USER_ROOT = "UserRoot";
-
-    private static final String KIND_FRIEND_LIST = "FriendList";
-    private static final String PROP_NAME_FRIEND_LIST = "friends";
-
-    private static final String KIND_LAST_KNOWN_POSITION = "LastKnownPosition";
-    private static final String PROP_NAME_LATITUDE = "latitude";
-    private static final String PROP_NAME_LONGITUDE = "longitude";
-    private static final String PROP_NAME_ACCURACY = "accuracy";
-    private static final String PROP_NAME_EPOCH_TIME = "epochTime";
 
     DatastoreService datastore;
 
@@ -29,7 +21,7 @@ class DataProvider {
 
     void addUser(String username) {
         // check whether user exists, throw if they do
-        Key userKey = KeyFactory.createKey(KIND_USER_ROOT, username);
+        Key userKey = KeyFactory.createKey(DatastoreNames.KIND_USER_ROOT, username);
         try {
             datastore.get(userKey);
             throw new IllegalArgumentException("already have user '" + username + "'");
@@ -42,7 +34,7 @@ class DataProvider {
     }
 
     String getUserList(String paramUsername) {
-        Query q = new Query(KIND_USER_ROOT);
+        Query q = new Query(DatastoreNames.KIND_USER_ROOT);
         PreparedQuery pq = datastore.prepare(q);
 
         StringBuilder buff = new StringBuilder();
@@ -59,11 +51,11 @@ class DataProvider {
 
     void addFriend(String username, String friend) {
         // get user
-        Key userRoot = KeyFactory.createKey(KIND_USER_ROOT, username);
+        Key userRoot = KeyFactory.createKey(DatastoreNames.KIND_USER_ROOT, username);
         Entity user = getUserEntity(userRoot);
 
         // get friend list child entity
-        Query q = new Query(KIND_FRIEND_LIST, userRoot);
+        Query q = new Query(DatastoreNames.KIND_FRIEND_LIST, userRoot);
         PreparedQuery pq = datastore.prepare(q);
         Entity friendsEntity = pq.asSingleEntity();
         if(friendsEntity == null) {
@@ -71,73 +63,73 @@ class DataProvider {
         }
 
         // retrieve current list and add new name
-        String friends = (String)friendsEntity.getProperty(PROP_NAME_FRIEND_LIST);
+        String friends = (String)friendsEntity.getProperty(DatastoreNames.PROP_FRIEND_LIST);
         if(friends.isEmpty()) {
             friends = friend;
         } else {
             friends += "," + friend;
         }
-        friendsEntity.setProperty(PROP_NAME_FRIEND_LIST, friends);
+        friendsEntity.setProperty(DatastoreNames.PROP_FRIEND_LIST, friends);
         datastore.put(friendsEntity);
     }
 
     String getFriendList(String username) {
         // get user
-        Key userRoot = KeyFactory.createKey(KIND_USER_ROOT, username);
+        Key userRoot = KeyFactory.createKey(DatastoreNames.KIND_USER_ROOT, username);
         Entity user = getUserEntity(userRoot);
 
         // get friend list child entity
-        Query q = new Query(KIND_FRIEND_LIST, userRoot);
+        Query q = new Query(DatastoreNames.KIND_FRIEND_LIST, userRoot);
         PreparedQuery pq = datastore.prepare(q);
         Entity friendsEntity = pq.asSingleEntity();
-        if(friendsEntity == null || !friendsEntity.hasProperty(PROP_NAME_FRIEND_LIST)) {
+        if(friendsEntity == null || !friendsEntity.hasProperty(DatastoreNames.PROP_FRIEND_LIST)) {
             friendsEntity = addDefaultFriendListEntity(user);
         }
-        return (String)friendsEntity.getProperty(PROP_NAME_FRIEND_LIST);
+        return (String)friendsEntity.getProperty(DatastoreNames.PROP_FRIEND_LIST);
     }
 
     void setLastKnownPosition(String username, LastKnownPosition pos) {
         // get user
-        Key userRoot = KeyFactory.createKey(KIND_USER_ROOT, username);
+        Key userRoot = KeyFactory.createKey(DatastoreNames.KIND_USER_ROOT, username);
         Entity user = getUserEntity(userRoot);
 
         // get last known position child entity
-        Query q = new Query(KIND_LAST_KNOWN_POSITION, userRoot);
+        Query q = new Query(DatastoreNames.KIND_LAST_KNOWN_POSITION, userRoot);
         PreparedQuery pq = datastore.prepare(q);
         Entity lkpEntity = pq.asSingleEntity();
         if(lkpEntity == null) {
             lkpEntity = addDefaultLastKnownPositionEntity(user);
         }
 
-        lkpEntity.setProperty(PROP_NAME_LATITUDE, pos.latitude);
-        lkpEntity.setProperty(PROP_NAME_LONGITUDE, pos.longitude);
-        lkpEntity.setProperty(PROP_NAME_ACCURACY, pos.accuracy);
-        lkpEntity.setProperty(PROP_NAME_EPOCH_TIME, pos.epochTime);
+        lkpEntity.setProperty(DatastoreNames.PROP_LATITUDE, pos.latitude);
+        lkpEntity.setProperty(DatastoreNames.PROP_LONGITUDE, pos.longitude);
+        lkpEntity.setProperty(DatastoreNames.PROP_ACCURACY, pos.accuracy);
+        lkpEntity.setProperty(DatastoreNames.PROP_EPOCH_TIME, pos.epochTime);
         datastore.put(lkpEntity);
     }
 
     LastKnownPosition getLastKnownPosition(String username) {
         // get user
-        Key userRoot = KeyFactory.createKey(KIND_USER_ROOT, username);
+        Key userRoot = KeyFactory.createKey(DatastoreNames.KIND_USER_ROOT, username);
         Entity user = getUserEntity(userRoot);
 
         // get last known position child entity
-        Query q = new Query(KIND_LAST_KNOWN_POSITION, userRoot);
+        Query q = new Query(DatastoreNames.KIND_LAST_KNOWN_POSITION, userRoot);
         PreparedQuery pq = datastore.prepare(q);
         Entity lkpEntity = pq.asSingleEntity();
         if(lkpEntity == null ||
-                !lkpEntity.hasProperty(PROP_NAME_LATITUDE) ||
-                !lkpEntity.hasProperty(PROP_NAME_LONGITUDE) ||
-                !lkpEntity.hasProperty(PROP_NAME_ACCURACY) ||
-                !lkpEntity.hasProperty(PROP_NAME_EPOCH_TIME)) {
+                !lkpEntity.hasProperty(DatastoreNames.PROP_LATITUDE) ||
+                !lkpEntity.hasProperty(DatastoreNames.PROP_LONGITUDE) ||
+                !lkpEntity.hasProperty(DatastoreNames.PROP_ACCURACY) ||
+                !lkpEntity.hasProperty(DatastoreNames.PROP_EPOCH_TIME)) {
             lkpEntity = addDefaultLastKnownPositionEntity(user);
         }
 
         LastKnownPosition pos = new LastKnownPosition();
-        pos.latitude = Double.parseDouble(lkpEntity.getProperty(PROP_NAME_LATITUDE).toString());
-        pos.longitude = Double.parseDouble(lkpEntity.getProperty(PROP_NAME_LONGITUDE).toString());
-        pos.accuracy = Double.parseDouble(lkpEntity.getProperty(PROP_NAME_ACCURACY).toString());
-        pos.epochTime = Long.parseLong(lkpEntity.getProperty(PROP_NAME_EPOCH_TIME).toString());
+        pos.latitude = Double.parseDouble(lkpEntity.getProperty(DatastoreNames.PROP_LATITUDE).toString());
+        pos.longitude = Double.parseDouble(lkpEntity.getProperty(DatastoreNames.PROP_LONGITUDE).toString());
+        pos.accuracy = Double.parseDouble(lkpEntity.getProperty(DatastoreNames.PROP_ACCURACY).toString());
+        pos.epochTime = Long.parseLong(lkpEntity.getProperty(DatastoreNames.PROP_EPOCH_TIME).toString());
         return pos;
     }
 
@@ -152,18 +144,18 @@ class DataProvider {
     }
 
     private Entity addDefaultLastKnownPositionEntity(Entity user) {
-        Entity positionEntity = new Entity(KIND_LAST_KNOWN_POSITION, user.getKey());
-        positionEntity.setProperty(PROP_NAME_LATITUDE, new Float(0.0));
-        positionEntity.setProperty(PROP_NAME_LONGITUDE, new Float(0.0));
-        positionEntity.setProperty(PROP_NAME_ACCURACY, new Float(0.0));
-        positionEntity.setProperty(PROP_NAME_EPOCH_TIME, new Long(0L));
+        Entity positionEntity = new Entity(DatastoreNames.KIND_LAST_KNOWN_POSITION, user.getKey());
+        positionEntity.setProperty(DatastoreNames.PROP_LATITUDE, new Float(0.0));
+        positionEntity.setProperty(DatastoreNames.PROP_LONGITUDE, new Float(0.0));
+        positionEntity.setProperty(DatastoreNames.PROP_ACCURACY, new Float(0.0));
+        positionEntity.setProperty(DatastoreNames.PROP_EPOCH_TIME, new Long(0L));
         datastore.put(positionEntity);
         return positionEntity;
     }
 
     private Entity addDefaultFriendListEntity(Entity user) {
-        Entity friendListEntity = new Entity(KIND_FRIEND_LIST, user.getKey());
-        friendListEntity.setProperty(PROP_NAME_FRIEND_LIST, "");
+        Entity friendListEntity = new Entity(DatastoreNames.KIND_FRIEND_LIST, user.getKey());
+        friendListEntity.setProperty(DatastoreNames.PROP_FRIEND_LIST, "");
         datastore.put(friendListEntity);
         return friendListEntity;
     }
--- server/HomingBaconServer/src/net/cruciblesoftware/homingbacon/server/HomingBaconServlet.java
+++ server/HomingBaconServer/src/net/cruciblesoftware/homingbacon/server/HomingBaconServlet.java
@@ -6,32 +6,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.cruciblesoftware.homingbacon.JsonKeys;
+import net.cruciblesoftware.homingbacon.JsonValues;
+import net.cruciblesoftware.homingbacon.ServerActions;
+import net.cruciblesoftware.homingbacon.UrlParameters;
+
 import com.google.gson.JsonObject;
 
 public class HomingBaconServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-
-    private static final String ADD_USER_ACTION = "adduser";
-    private static final String ADD_FRIEND_ACTION = "addfriend";
-    private static final String GET_FRIEND_LIST_ACTION = "getfriendlist";
-    private static final String GET_POSITION_ACTION = "getposition";
-    private static final String SET_POSITION_ACTION = "setposition";
-    private static final String GET_USERS_ACTION = "getusers";
-
-    private static final String JSON_KEY_STATUS = "status";
-    private static final String JSON_KEY_MESSAGE = "message";
-    private static final String JSON_KEY_LAT = "latitude";
-    private static final String JSON_KEY_LON = "longitude";
-    private static final String JSON_KEY_ACCURACY = "accuracy";
-    private static final String JSON_KEY_TIME = "epoch_time";
-    private static final String JSON_VALUE_SUCCESS = "success";
-    private static final String JSON_VALUE_ERROR = "error";
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
         doGet(req, resp);
     }
+
+    /*
+     * 
+    // UserService userServ = UserServiceFactory.getUserService();
+    // User user = userServ.getCurrentUser();
+     * if(user == null) {
+     * resp.sendRedirect(userServ.createLoginURL(req.getRequestURI())); }
+     * else { writer.println("user nickname=" + user.getNickname());
+     * writer.println("user id=" + user.getUserId());
+     * writer.println("user email=" + user.getEmail()); }
+     */
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -41,67 +41,45 @@ public class HomingBaconServlet extends HttpServlet {
         JsonObject json = new JsonObject();
 
         // basic GET params
-        String paramAction = req.getParameter("action");
-        String paramUsername = req.getParameter("username");
+        String paramAction = null;
+        String paramUsername = null;
 
         try {
             // check basic params
-            if(paramAction == null) {
-                throw new IllegalArgumentException("missing 'action' parameter");
-            }
-            if(paramUsername == null) {
-                throw new IllegalArgumentException("missing 'username' parameter");
-            }
+            paramAction = getUrlParam(req, UrlParameters.ACTION);
+            paramUsername = getUrlParam(req, UrlParameters.USERNAME);
 
             // parse action
             DataProvider data = new DataProvider();
-            if(paramAction.equalsIgnoreCase(ADD_USER_ACTION)) {
+            if(paramAction.equalsIgnoreCase(ServerActions.ADD_USER)) {
                 data.addUser(paramUsername);
-                json.addProperty(JSON_KEY_STATUS, JSON_VALUE_SUCCESS);
-            } else if(paramAction.equalsIgnoreCase(GET_USERS_ACTION)) {
+                json.addProperty(JsonKeys.STATUS, JsonValues.SUCCESS);
+            } else if(paramAction.equalsIgnoreCase(ServerActions.GET_USERS)) {
                 String users = data.getUserList(paramUsername);
-                json.addProperty("user_list", users);
-                json.addProperty(JSON_KEY_STATUS, JSON_VALUE_SUCCESS);
-            } else if(paramAction.equalsIgnoreCase(GET_FRIEND_LIST_ACTION)) {
+                json.addProperty(JsonKeys.USER_LIST, users);
+                json.addProperty(JsonKeys.STATUS, JsonValues.SUCCESS);
+            } else if(paramAction.equalsIgnoreCase(ServerActions.GET_FRIEND_LIST)) {
                 String friends = data.getFriendList(paramUsername);
-                json.addProperty("friend_list", friends);
-                json.addProperty(JSON_KEY_STATUS, JSON_VALUE_SUCCESS);
-            } else if(paramAction.equalsIgnoreCase(ADD_FRIEND_ACTION)) {
-                String paramFriend = req.getParameter("friend");
-                if(paramFriend == null) {
-                    throw new IllegalArgumentException("missing 'friend' parameter");
-                }
+                json.addProperty(JsonKeys.FRIEND_LIST, friends);
+                json.addProperty(JsonKeys.STATUS, JsonValues.SUCCESS);
+            } else if(paramAction.equalsIgnoreCase(ServerActions.ADD_FRIEND)) {
+                String paramFriend = getUrlParam(req, UrlParameters.FRIEND);
                 data.addFriend(paramUsername, paramFriend);
-                json.addProperty(JSON_KEY_STATUS, JSON_VALUE_SUCCESS);
-            } else if(paramAction.equalsIgnoreCase(GET_POSITION_ACTION)) {
-                String paramFriend = req.getParameter("friend");
-                if(paramFriend == null) {
-                    throw new IllegalArgumentException("missing 'friend' parameter");
-                }
+                json.addProperty(JsonKeys.STATUS, JsonValues.SUCCESS);
+            } else if(paramAction.equalsIgnoreCase(ServerActions.GET_POSITION)) {
+                String paramFriend = getUrlParam(req, UrlParameters.FRIEND);
                 LastKnownPosition pos = data.getLastKnownPosition(paramFriend);
-                json.addProperty(JSON_KEY_LAT, pos.latitude);
-                json.addProperty(JSON_KEY_LON, pos.longitude);
-                json.addProperty(JSON_KEY_ACCURACY, pos.accuracy);
-                json.addProperty(JSON_KEY_TIME, pos.epochTime);
-                json.addProperty(JSON_KEY_STATUS, JSON_VALUE_SUCCESS);
-            } else if(paramAction.equalsIgnoreCase(SET_POSITION_ACTION)) {
+                json.addProperty(JsonKeys.LATITUDE, pos.latitude);
+                json.addProperty(JsonKeys.LONGITUDE, pos.longitude);
+                json.addProperty(JsonKeys.ACCURACY, pos.accuracy);
+                json.addProperty(JsonKeys.EPOCH_TIME, pos.epochTime);
+                json.addProperty(JsonKeys.STATUS, JsonValues.SUCCESS);
+            } else if(paramAction.equalsIgnoreCase(ServerActions.SET_POSITION)) {
                 // first get and check the URL parameters
-                String paramLat = req.getParameter("lat");
-                if(paramLat == null) {
-                    throw new IllegalArgumentException("missing 'lat' parameter");
-                }
-                String paramLon = req.getParameter("lon");
-                if(paramLon == null) {
-                    throw new IllegalArgumentException("missing 'lon' parameter");
-                }
-                String paramAccuracy = req.getParameter("accuracy");
-                if(paramAccuracy == null) {
-                    throw new IllegalArgumentException("missing 'accuracy' parameter");
-                }
-                String paramTime = req.getParameter("time");
-                if(paramTime == null) {
-                    throw new IllegalArgumentException("missing 'time' parameter");
-                }
+                String paramLat = getUrlParam(req, UrlParameters.LATITUDE);
+                String paramLon = getUrlParam(req, UrlParameters.LONGITUDE);
+                String paramAccuracy = getUrlParam(req, UrlParameters.ACCURACY);
+                String paramTime = getUrlParam(req, UrlParameters.EPOCH_TIME);
 
                 // create a position and set it
                 LastKnownPosition pos = new LastKnownPosition();
@@ -110,34 +88,29 @@ public class HomingBaconServlet extends HttpServlet {
                 pos.accuracy = Double.parseDouble(paramAccuracy);
                 pos.epochTime = Long.parseLong(paramTime);
                 data.setLastKnownPosition(paramUsername, pos);
-                json.addProperty(JSON_KEY_STATUS, JSON_VALUE_SUCCESS);
+                json.addProperty(JsonKeys.STATUS, JsonValues.SUCCESS);
             }
         } catch(NumberFormatException e) {
-            json.addProperty(JSON_KEY_STATUS, JSON_VALUE_ERROR);
-            json.addProperty(JSON_KEY_MESSAGE, "could not format number: " + e.getLocalizedMessage());
+            json.addProperty(JsonKeys.STATUS, JsonValues.ERROR);
+            json.addProperty(JsonKeys.MESSAGE, "could not format number: " + e.getLocalizedMessage());
         } catch(IllegalArgumentException e) {
-            json.addProperty(JSON_KEY_STATUS, JSON_VALUE_ERROR);
-            json.addProperty(JSON_KEY_MESSAGE, "bad argument: " + e.getLocalizedMessage());
+            json.addProperty(JsonKeys.STATUS, JsonValues.ERROR);
+            json.addProperty(JsonKeys.MESSAGE, "bad argument: " + e.getLocalizedMessage());
         } catch(Exception e) {
-            json.addProperty(JSON_KEY_STATUS, JSON_VALUE_ERROR);
-            json.addProperty(JSON_KEY_MESSAGE, "unknown error: " + e.getLocalizedMessage());
+            json.addProperty(JsonKeys.STATUS, JsonValues.ERROR);
+            json.addProperty(JsonKeys.MESSAGE, "unknown error: " + e.getLocalizedMessage());
             e.printStackTrace(System.err);
         }
 
         resp.getWriter().println(json.toString());
-
-
-
-        /*
-         * 
-        // UserService userServ = UserServiceFactory.getUserService();
-        // User user = userServ.getCurrentUser();
-         * if(user == null) {
-         * resp.sendRedirect(userServ.createLoginURL(req.getRequestURI())); }
-         * else { writer.println("user nickname=" + user.getNickname());
-         * writer.println("user id=" + user.getUserId());
-         * writer.println("user email=" + user.getEmail()); }
-         */
     }
 
+    private String getUrlParam(HttpServletRequest req, String name)
+            throws IllegalArgumentException {
+        String val = req.getParameter(name);
+        if(val == null) {
+            throw new IllegalArgumentException(String.format(JsonValues.MISSING_PARAM, name));
+        }
+        return val;
+    }
 }
